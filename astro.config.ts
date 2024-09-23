77e820011fbcd47a48a89802cc49ddae203a2bd8
Maulik Jadav <admin@stagewise.app>
2024-09-23 14:31:49 +0530

    Trying github pageS

--- astro.config.ts
+++ astro.config.ts
@@ -23,7 +23,7 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
 
 export default defineConfig({
 
-    site: 'https://pegasus-automation.github.io',
+    // site: 'https://pegasus-automation.github.io',
 
     output: 'static',
 
